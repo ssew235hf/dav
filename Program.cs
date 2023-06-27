cde33f3f7b11ccb47e6fffe6ba73624d23bc3e18
Nikita <skyrimlau@gmail.com>
2023-06-27 12:30:33 +0300

    added README.md

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("Первое число больше второго");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine("Первое число меньше второго");
+            }
+            else
+            {
+                Console.WriteLine("Оба числа равны");
+            }
+            Console.ReadKey();
+        }
+    }
+}
